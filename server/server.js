0b8506acf1a382266d9f9b96e06b0d468734524d
younger9403 <younger9403@gmail.com>
2020-02-03 19:32:52 +0900

    ++ React - Node.js 웹소켓 연결 완료

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const cors = require('cors')
+
+app.use(cors());
+server.listen(4000, () => console.log('server is running on port 4000...'))
+
+const socketio = require('socket.io')();
+const io = socketio.listen(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('init', (data) => {
+    console.log(data.name);
+    socket.emit('welcome', `hello! ${data.name} your socket id is ${socket.id}`);
+  })
+})
+
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import socketio from 'socket.io-client';
+const socket = socketio.connect('http://localhost:4000');
+(() => {
+  socket.emit('init', {name: 'yuna'});
+  socket.on('welcome', (msg) => {
+    console.log(msg);
+  })
+})();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
